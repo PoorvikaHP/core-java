85935c6e4a0d52fe527c585dfbd0813fbce66c7d
unknown <poorvikahp81@gmail.com>
2026-04-05 12:33:28 +0530

    initailizing variable

---
+++ LaptopRunner.java
@@ -0,0 +1,11 @@
+class LaptopRunner{
+    public static void main(String[] args) {
+Laptop l1 = new Laptop();
+System.out.println("-");
+Laptop l2 = new Laptop();
+  System.out.println("-");
+Laptop l4 = new Laptop();
+ System.out.println("-");
+Laptop l5 = new Laptop();
+    }
+}
